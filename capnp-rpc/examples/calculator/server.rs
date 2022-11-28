capnproto/capnproto-rust

remove redundant field names

capnp-rpc/examples/calculator/server.rs
@@ -36,7 +36,7 @@ struct ValueImpl {
 
 impl ValueImpl {
     fn new(value: f64) -> ValueImpl {
-        ValueImpl { value: value }
+        ValueImpl { value }
     }
 }
 
@@ -101,7 +101,7 @@ impl FunctionImpl {
         body: calculator::expression::Reader,
     ) -> ::capnp::Result<FunctionImpl> {
         let mut result = FunctionImpl {
-            param_count: param_count,
+            param_count,
             body: ::capnp_rpc::ImbuedMessageBuilder::new(::capnp::message::HeapAllocator::new()),
         };
         result.body.set_root(body)?;
@@ -199,7 +199,7 @@ impl calculator::Server for CalculatorImpl {
         let op = pry!(pry!(params.get()).get_op());
         results
             .get()
-            .set_func(capnp_rpc::new_client(OperatorImpl { op: op }));
+            .set_func(capnp_rpc::new_client(OperatorImpl { op }));
         Promise::ok(())
     }
 }

capnp-rpc/examples/pubsub/server.rs
@@ -54,10 +54,7 @@ struct SubscriptionImpl {
 
 impl SubscriptionImpl {
     fn new(id: u64, subscribers: Rc<RefCell<SubscriberMap>>) -> SubscriptionImpl {
-        SubscriptionImpl {
-            id: id,
-            subscribers: subscribers,
-        }
+        SubscriptionImpl { id, subscribers }
     }
 }
 

capnp-rpc/test/impls.rs
@@ -544,7 +544,7 @@ impl Handle {
     fn new(count: &Rc<Cell<i64>>) -> Handle {
         let count = count.clone();
         count.set(count.get() + 1);
-        Handle { count: count }
+        Handle { count }
     }
 }
 

Fundament-Software/capstone-rs

capnp-rpc/examples/calculator/server.rs
@@ -36,7 +36,7 @@ struct ValueImpl {
 
 impl ValueImpl {
     fn new(value: f64) -> Self {
-        Self { value: value }
+        Self { value }
     }
 }
 
@@ -98,7 +98,7 @@ struct FunctionImpl {
 impl FunctionImpl {
     fn new(param_count: u32, body: calculator::expression::Reader) -> ::capnp::Result<Self> {
         let mut result = Self {
-            param_count: param_count,
+            param_count,
             body: ::capnp_rpc::ImbuedMessageBuilder::new(::capnp::message::HeapAllocator::new()),
         };
         result.body.set_root(body)?;
@@ -196,7 +196,7 @@ impl calculator::Server for CalculatorImpl {
         let op = pry!(pry!(params.get()).get_op());
         results
             .get()
-            .set_func(capnp_rpc::new_client(OperatorImpl { op: op }));
+            .set_func(capnp_rpc::new_client(OperatorImpl { op }));
         Promise::ok(())
     }
 }

capnp-rpc/examples/pubsub/server.rs
@@ -54,10 +54,7 @@ struct SubscriptionImpl {
 
 impl SubscriptionImpl {
     fn new(id: u64, subscribers: Rc<RefCell<SubscriberMap>>) -> Self {
-        Self {
-            id: id,
-            subscribers: subscribers,
-        }
+        Self { id, subscribers }
     }
 }
 

capnp-rpc/test/impls.rs
@@ -544,7 +544,7 @@ impl Handle {
     fn new(count: &Rc<Cell<i64>>) -> Self {
         let count = count.clone();
         count.set(count.get() + 1);
-        Self { count: count }
+        Self { count }
     }
 }
 