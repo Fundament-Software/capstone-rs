5e44cd2ee6d2ee80e2b0b7500a6cdd6f7bffb914
nivereno@gmail.com <nivereno@gmail.com>
2024-08-07 17:01:22 +0300

    Trying stuff

--- capnp/src/capability.rs
+++ capnp/src/capability.rs
@@ -403,6 +403,8 @@ impl crate::introspect::Introspect for Client {
     fn introspect() -> crate::introspect::Type {
         crate::introspect::TypeVariant::Capability(crate::introspect::RawCapabilitySchema {
             encoded_node: &[],
+            params_types: &[],
+            result_types: &[],
         })
         .into()
     }
