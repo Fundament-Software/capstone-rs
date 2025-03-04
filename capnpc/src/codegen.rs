1eca4971d9052086d367c36c4ca4eeb7691572ef
nivereno@gmail.com <nivereno@gmail.com>
2025-03-04 09:09:42 +0300

    fmt

--- capnpc/src/codegen.rs
+++ capnpc/src/codegen.rs
@@ -1555,51 +1555,49 @@ fn check_fields_of_struct_for_lifetimes<'a>(
                     *lifetime = "'a, ";
                     return Ok(());
                 }
-                type_::Which::List(mut l) => {
-                    loop {
-                        match l.get_element_type()?.which()? {
-                            type_::Which::Text(_) => {
+                type_::Which::List(mut l) => loop {
+                    match l.get_element_type()?.which()? {
+                        type_::Which::Text(_) => {
+                            *lifetime = "'a, ";
+                            return Ok(());
+                        }
+                        type_::Which::Data(_) => {
+                            *lifetime = "'a, ";
+                            return Ok(());
+                        }
+                        type_::Which::Struct(inner) => {
+                            let capnp::schema_capnp::node::Struct(struct_node) =
+                                ctx.node_map[&inner.get_type_id()].which()?
+                            else {
+                                return Err(capnp::Error::failed("Type mismatch".to_string()));
+                            };
+                            if !get_params(ctx, inner.get_type_id())?.is_empty() {
                                 *lifetime = "'a, ";
                                 return Ok(());
-                            },
-                            type_::Which::Data(_) => {
-                                *lifetime = "'a, ";
-                                return Ok(());
-                            },
-                            type_::Which::Struct(inner) => {
-                                let capnp::schema_capnp::node::Struct(struct_node) =
-                                    ctx.node_map[&inner.get_type_id()].which()?
-                                else {
-                                    return Err(capnp::Error::failed("Type mismatch".to_string()));
-                                };
-                                if !get_params(ctx, inner.get_type_id())?.is_empty() {
-                                    *lifetime = "'a, ";
-                                    return Ok(());
-                                }
-                                if maybe_cyclical_counter > 10 {
-                                    break;
-                                }
-                                maybe_cyclical_counter += 1;
-                                check_fields_of_struct_for_lifetimes(
-                                    ctx,
-                                    struct_node.get_fields()?,
-                                    lifetime,
-                                    maybe_cyclical_counter,
-                                )?;
                             }
-                            type_::Which::AnyPointer(_) => {
-                                if slot.get_type()?.is_parameter()? {
-                                    *lifetime = "'a, ";
-                                    return Ok(());
-                                }
-                            }
-                            type_::Which::List(inner) => {
-                                l = inner;
-                                continue;
-                            }
-                            _ => {
+                            if maybe_cyclical_counter > 10 {
                                 break;
                             }
+                            maybe_cyclical_counter += 1;
+                            check_fields_of_struct_for_lifetimes(
+                                ctx,
+                                struct_node.get_fields()?,
+                                lifetime,
+                                maybe_cyclical_counter,
+                            )?;
+                        }
+                        type_::Which::AnyPointer(_) => {
+                            if slot.get_type()?.is_parameter()? {
+                                *lifetime = "'a, ";
+                                return Ok(());
+                            }
+                        }
+                        type_::Which::List(inner) => {
+                            l = inner;
+                            continue;
+                        }
+                        _ => {
+                            break;
                         }
                     }
                 },
