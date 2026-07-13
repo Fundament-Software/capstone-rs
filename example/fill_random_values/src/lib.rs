654321baa43e3c2e31899495a836d241fc663f1a
Erik McClure <erikm@fundament.software>
2026-07-13 02:18:18 -0700

    deduplicate crates and move to papaya

--- example/fill_random_values/src/lib.rs
+++ example/fill_random_values/src/lib.rs
@@ -4,7 +4,6 @@ use rand::distr::uniform::SampleUniform;
 use capnp::introspect::TypeVariant;
 use capnp::schema;
 use capnp::{dynamic_struct, dynamic_value};
-use rand::RngExt;
 
 pub mod fill_capnp {
     include!(concat!(env!("OUT_DIR"), "/fill_capnp.rs"));
